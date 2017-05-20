a2c0e0b375db3b293f674a4a2365a6181cd044d7
Noturno <Noturno@192.168.7.12>
2017-05-19 21:56:32 -0300

    Hora de dormir!

---
+++ Algoritimos/Exercicio02/exercicio/numero1items/Classe.java
@@ -0,0 +1,13 @@
+package exercicio.numero1items;
+
+	public class Classe {
+		public static void main(String[] args) {
+			for (int aux = 1; aux <= 100; aux++) {
+
+					System.out.println(aux*aux);
+				}
+			}
+
+		
+			
+}
